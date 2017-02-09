341391707e3102d77690bfd51ade8f70e2336214
jianming.zhou <jianming.zhou@ele.me>
2017-02-09 16:29:41 +0800

    updateVersion.sh简化

--- z-sky-core/updateVersion.sh
+++ z-sky-core/updateVersion.sh
@@ -3,6 +3,3 @@ VERSION=1.0.0-SNAPSHOT
 
 mvn versions:set -DnewVersion=${VERSION}
 find ../ -name pom.xml.versionsBackup | xargs rm -rf
-
-echo "\n"
-echo "【注意】如无特殊版本需要，请将当前目录pom.xml下的properties[z-sky.version]手动改为${VERSION} ! \n"
